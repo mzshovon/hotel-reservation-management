c30e013623353b8c494762b9f747246ca01644f7
farhad681 <mohammadfarhad681@gmail.com>
2023-08-12 15:40:38 +0600

    admin dashboard mastering

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'App\Http\Controllers\admin'], function() {
+    Route::get('/admin/dashboard', 'HomeController@dashboard')->name('admin.dashboard');
+});
