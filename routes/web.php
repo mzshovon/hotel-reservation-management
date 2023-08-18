970f83b035694fa8a8f4de8f18ae6c74da954cc2
Mohammad Moniruzzaman <mohammad.moniruzzaman@brainstation-23.com>
2023-08-18 16:59:54 +0600

    Route for hotel, room and utility added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Frontend\HotelController;
 use App\Http\Controllers\Frontend\LandingPageController;
+use App\Http\Controllers\Frontend\UtilityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [LandingPageController::class, 'viewLandingPage'])->name("landingPage");
+Route::get('/contact-us', [UtilityController::class, 'contactUs'])->name("contactUs");
+Route::post('/contact-us', [UtilityController::class, 'storeContactUs'])->name("storeContactUs");
+Route::get('/about-us', [UtilityController::class, 'aboutUs'])->name("aboutUs");
+Route::get('/news', [UtilityController::class, 'news'])->name("news");
+Route::get('/news/{id}', [UtilityController::class, 'singleNews'])->name("singleNews");
+
+//Hotel
+Route::get('/hotels', [HotelController::class, 'viewHotelsPage'])->name("hotels");
+Route::get('/hotels/{hotelId}', [HotelController::class, 'viewSingleHotelInfo'])->name("singleHotelInfo");
+Route::get('/rooms', [HotelController::class, 'viewRoomsPage'])->name("rooms");
+Route::get('/rooms/{roomId}', [HotelController::class, 'viewSingleRoomInfo'])->name("singleRoomInfo");
