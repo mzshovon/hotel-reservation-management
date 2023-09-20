9d5b81e32020c7e957eae06ddfab1529fc18d2f6
Mohammad Moniruzzaman <mohammad.moniruzzaman@brainstation-23.com>
2023-09-20 18:33:58 +0600

    Redis with activity log added

--- config/database.php
+++ config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'redis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', false),
